8e2d3cb0f01f8dd52646b0e136cac2c8f05d92c4
atichapre <aticha.pre@gmail.com>
2025-05-06 00:25:25 +0700

    feat: change Note schema for timestamp

--- models/Note.js
+++ models/Note.js
@@ -1,12 +1,20 @@
 import { Schema, model, mongoose } from "mongoose";
 
-const NoteSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  tags: { type: [String], default: [] },
-  isPinned: { type: Boolean, default: false },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  createdAt: { type: Date, default: Date.now() },
-});
+const NoteSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: { type: [String], default: [] },
+    isPinned: { type: Boolean, default: false },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+// createdAt: { type: Date, default: Date.now() },
+// updatedAt: { type: Date, default: new Date() },
 
 export const Note = model("Note", NoteSchema);
